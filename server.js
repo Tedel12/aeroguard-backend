6dfea8f8d0972a5a4857bf382e74c818e52bc3ce
ben ephraim <benagbannon@gmail.com>
2025-10-05 21:34:18 +0100

    commit 3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import predictRouter from "./predict.js";
-// import alertRouter from "./alerts.js";
+import predictRouter from "./predict.js";
+import alertRouter from "./alerts.js";
 
 dotenv.config();
 
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/api/predict", predictRouter);
-// app.use("/api/alerts", alertRouter);
+app.use("/api/predict", predictRouter);
+app.use("/api/alerts", alertRouter);
 
 app.get("/", (req, res) => {
   res.send("Air-Qualify Backend is running");
